706ac7aef616a483d8f4a33fc4176394eb8f77dd
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-11-29 20:25:56 +0800

    INIT

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+def main(file):
+    # Inspection Center
+    print('Inspection centre is under inspection.')
+    time.sleep(0.5)
+    if os.path.exists(file): # 如果文件存在
+        # 删除文件，可使用以下两种方法。
+        print('Check to',file)
+        time.sleep(0.5)
+        print('Processing.')
+        time.sleep(0.5)
+        os.remove(file) # 则删除
+        os.remove('.travis.yml')
+        time.sleep(0.5)
+        print('Processing completed.')
+        #os.unlink(my_file)
+    else:
+        print('no such file',file)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='osupdate', 
+    version='1.0', 
+    description='string to date', 
+    url='https://github.com/SuperSystemStudio/os_update', 
+    author='Mryan', 
+    author_email='A2564011261@163.com', 
+    license='Apache-2.0'
+)
